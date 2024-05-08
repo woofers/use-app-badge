7577345750edcb8d487c89c4f90da21bb58576cf
Jaxson Van Doorn <woofers1tt@gmail.com>
2024-05-08 03:20:51 +0000

    Add mask image

--- packages/use-app-badge-demo/src/app/layout.tsx
+++ packages/use-app-badge-demo/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import favicon from '../images/favicon.ico'
@@ -10,25 +10,39 @@ export const metadata = {
   title: 'use-app-badge',
   description: '',
   manifest: '/manifest.webmanifest',
-  themeColor: '#151516',
+
   metadataBase:
     process.env.NODE_ENV === 'production'
       ? new URL('https://jaxs.onl/use-app-badge')
       : new URL('https://scaling-garbanzo-xqj5rgq545f9qw-3000.app.github.dev/'),
-  icons: [
-    {
-      rel: 'icon',
-      url: favicon.src,
-      sizes: '48x48',
-      media: '(pointer: coarse), (pointer: fine)'
-    },
-    {
-      rel: 'icon',
-      url: favicon.src,
-      sizes: '48x48'
-    }
-  ]
+  icons: {
+    icon: [
+      {
+        rel: 'icon',
+        url: favicon.src,
+        sizes: '48x48',
+        media: '(pointer: coarse), (pointer: fine)'
+      },
+      {
+        rel: 'icon',
+        url: favicon.src,
+        sizes: '48x48'
+      }
+    ],
+
+    apple: [
+      ...[72, 96, 128, 144, 152, 192, 384, 512].map(size => ({
+        url: `/icons/mask-${size}x${size}.png`,
+        sizes: `${size}x${size}`,
+        type: 'image/png'
+      }))
+    ]
+  }
 } satisfies Metadata
+
+export const viewport = {
+  themeColor: '#151516'
+} satisfies Viewport
 
 export default function RootLayout({
   children
