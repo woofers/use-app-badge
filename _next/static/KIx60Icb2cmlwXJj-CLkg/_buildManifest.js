668a03fceb43dc8f55dedaa29afc174f217ab333
woofers <woofers1tt@gmail.com>
2024-08-13 01:27:47 +0000

    Updates

---
+++ _next/static/KIx60Icb2cmlwXJj-CLkg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-c06d9c7f0a81be54.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/chunks/pages/_app-0679c4a4f7d3cc44.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{3239:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(8275)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[774,179],()=>(n(3239),n(301))),_N_E=_.O()}]);
